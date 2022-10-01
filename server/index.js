b96c144c4e9d62536c1d266e85c08a294b932d03
Daniel Haycraft <dhcopy1@gmail.com>
2022-10-01 13:29:13 -0700

    lol this is hard

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const { getAllHouses, deleteHouse, createHouse, updateHouse } = require('./controller');
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/api/houses', getAllHouses);
+app.delete('/api/houses/:id', deleteHouse);
+app.post('/api/houses', createHouse);
+app.put('/api/houses/:id', updateHouse);
+
+app.listen(4004),() => console.log('server is 4004')
+
